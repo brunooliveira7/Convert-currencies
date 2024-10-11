dcb961b41db835d324e3790177e443614f1a7c40
Bruno Oliveira <brunoroliveira7@gmail.com>
2024-10-11 20:29:50 -0300

    get the selected currency

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 const amount = document.getElementById("amount");
+const currency = document.getElementById("currency");
+const form = document.querySelector("form");
 
-//input - capta o valor digita no input
+//input - capta o valor digitado no input
 //manipular o input para receber somente números
 amount.addEventListener("input", () => {
   //verifica se o valor é padrão número
@@ -9,5 +11,10 @@ amount.addEventListener("input", () => {
   //replace - substitui os caracteres não-numéricos por vazio
   //não permite digitar caracteres
   amount.value = amount.value.replace(hasCharacterRegex, "");
-  
 });
+
+//Capta o evento de submit (enviar) do form
+form.onsubmit = (event) => {
+  event.preventDefault();
+  console.log(currency.value);
+};
